e8edf9a81c54b252c62fa62595d1a2331ddc3e1d
hzrncik <hzrncik@redhat.com>
2021-06-30 16:47:38 +0200

    action with docker

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/hello
 
 go 1.16
+
+require github.com/testcontainers/testcontainers-go v0.11.1 // indirect
--- testing/my_test.go
+++ testing/my_test.go
@@ -2,12 +2,31 @@ package test
 
 import (
 	"fmt"
+	"github.com/testcontainers/testcontainers-go"
+	"log"
 	"os"
 	"testing"
 )
 
 func TestT1(t *testing.T) {
+	t.Log("Test zacina")
+	var err error
 	fmt.Println("t1")
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	path := dir + "/compose-kafka-zookeeper.yml"
+	var tc = testcontainers.NewLocalDockerCompose([]string{path}, "someName2")
+	var res = tc.WithCommand([]string{"up", "-d"}).Invoke()
+	if res.Error != nil {
+		log.Println("DORITI")
+		fmt.Println("do you feel it")
+		t.Error("container didn't start")
+	}
+	log.Println("docker compose networka created")
+
+	tc.Down()
 	//t.Error("test TestT1 sadly failed")
 }
 
@@ -23,5 +42,6 @@ func TestT3(t *testing.T) {
 
 func TestMain(m *testing.M) {
 	code := m.Run()
+
 	os.Exit(code)
 }
